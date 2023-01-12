e9d183749cfa20891a30d15a81c3a87e2feb7322
hairyElephant <user@email.com>
2023-01-12 17:00:28 -0500

    done

--- src/App.js
+++ src/App.js
@@ -1,37 +1,59 @@
-import './App.css';
-import { useEffect, useState } from 'react';
-import Banner from "./Banner.js"
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
-  // const [characters, setCharacters] = useState([])
-  // const [person, setPerson] = useState({})
-  // // state for hiding information in accordion
-  // const [display, setDisplay] = useState(false)
-  
-  // function apiCall(){
-  //   fetch('https://rickandmortyapi.com/api/character')
-  //   .then(res => res.json())
-  //   .then(data => setCharacters(data.results))
-  // }
+  const [characters, setCharacters] = useState([]);
+  // const [character, setCharacter] = useState({});
+  const [charName, setCharName] = useState("");
+  // state for hiding information in accordion
+  const [display, setDisplay] = useState(false);
 
-  // useEffect(() => {
-  //   apiCall()
-  // },[])
+  function apiCall() {
+    fetch("https://rickandmortyapi.com/api/character")
+      .then((res) => res.json())
+      .then((data) => setCharacters(data.results));
+  }
 
-  // function toggle(){
-  //   setDisplay(prev => !prev)
-  // }
-  // function handleClick(charInfo){
-  //   setPerson(charInfo)
-  //   toggle()
-  // }
+  useEffect(() => {
+    apiCall();
+  }, []);
 
+  function toggle() {
+    setDisplay((prev) => !prev);
+  }
+  function handleClick(charInfo) {
+    // setCharacter(charInfo);
+    setCharName(charInfo.name);
+    toggle();
+  }
 
-  // console.log(characters)
+  console.log(characters);
 
   return (
     <div className="App">
-     <Banner/>
+      <div>
+        {characters.length &&
+          characters.map((character, index) => (
+            <div>
+              <div className="banner">
+                <h2 onClick={() => handleClick(character)} key={index}>
+                  {character.name}
+                </h2>
+              </div>
+              {display && character.name === charName && (
+                <div className="xtraInfo">
+                  <img src={character.image} />
+                  <p>
+                    Status: {character.status} <br /> Origin:{" "}
+                    {character.origin.name} <br /> Species: {character.species}{" "}
+                    <br />
+                    Gender: {character.gender}{" "}
+                  </p>
+                </div>
+              )}
+            </div>
+          ))}
+      </div>
     </div>
   );
 }
