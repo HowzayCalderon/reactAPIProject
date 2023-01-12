b99268cfab306e555bc2d41e43871a9e1636ea52
hairyElephant <user@email.com>
2023-01-12 16:33:57 -0500

    public

--- src/App.js
+++ src/App.js
@@ -1,49 +1,37 @@
 import './App.css';
 import { useEffect, useState } from 'react';
+import Banner from "./Banner.js"
 
 function App() {
-  const [characters, setCharacters] = useState([])
-  const [character, setCharacter] = useState({})
-  // state for hiding information in accordion
-  const [display, setDisplay] = useState(false)
+  // const [characters, setCharacters] = useState([])
+  // const [person, setPerson] = useState({})
+  // // state for hiding information in accordion
+  // const [display, setDisplay] = useState(false)
   
-  function apiCall(){
-    fetch('https://rickandmortyapi.com/api/character')
-    .then(res => res.json())
-    .then(data => setCharacters(data.results))
-  }
+  // function apiCall(){
+  //   fetch('https://rickandmortyapi.com/api/character')
+  //   .then(res => res.json())
+  //   .then(data => setCharacters(data.results))
+  // }
 
-  useEffect(() => {
-    apiCall()
-  },[])
+  // useEffect(() => {
+  //   apiCall()
+  // },[])
 
-  function toggle(){
-    setDisplay(prev => !prev)
-  }
-  function handleClick(charInfo){
-    setCharacter(charInfo)
-    toggle()
-  }
+  // function toggle(){
+  //   setDisplay(prev => !prev)
+  // }
+  // function handleClick(charInfo){
+  //   setPerson(charInfo)
+  //   toggle()
+  // }
 
 
-  console.log(characters)
+  // console.log(characters)
 
   return (
     <div className="App">
-      <div>
-      {characters.length && characters.map((character, index) =>(
-        <div>
-          <div className='banner'>
-            <h2>{character.name}<button onClick={()=> handleClick(character)} key={index}>ˇ</button></h2>
-          </div>
-          {display?<div className='xtraInfo'>
-            <img src={character.image}/>
-            <p>Status: {character.status} <br/> Origin: {character.origin.name} <br/> Species: {character.species} <br/>Gender: {character.gender} </p>
-          </div>: null}  
-        </div>
-        
-    ))}
-      </div>
+     <Banner/>
     </div>
   );
 }
